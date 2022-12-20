d30ca9b15c9a12deb9e74a3350ad4711cfde1d2a
yuvraj <yuvarma07@gmail.com>
2022-12-20 11:28:57 +0530

    Reduced LOC

--- server/index.js
+++ server/index.js
@@ -5,9 +5,9 @@ const app = express();
 app.get('/', (request, response) => response.sendFile( __dirname + '/index.html'));
 app.post('/',function (request, response) {
 
-    var lovedata = Math.random() * 100;
-    lovdata = Math.floor(lovedata);
-    response.send('<br><br><br><br><center><h1>' + 'Your love percentage is    : ' + lovdata + ' %' + '</h></center>') 
+    /* var lovedata = Math.random() * 100;
+    lovdata = Math.floor(lovedata); */
+    response.send('<br><br><br><br><center><h1>' + 'Your love percentage is    : ' + Math.floor(Math.random() * 100) + ' %' + '</h></center>') 
 })
 
 app.listen(8000, () => console.log("Server is running on port 800")); 
