73073abb5799a06b9a00f4a476142783ded65e2a
yuvraj <yuvarma07@gmail.com>
2022-12-20 10:59:30 +0530

    Removed unused imports

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended : true}))
 
 app.get('/', (request, response) => response.sendFile( __dirname + '/index.html'));
 app.post('/',function (request, response) {
